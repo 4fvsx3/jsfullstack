9fb91d425e872043c4cee301f08fed42c1764c43
4fvsx3 <4fvsx3@gmail.com>
2025-09-17 12:42:38 +0900

    Update gatsby-node.js

--- coronaboard-web/gatsby-node.js
+++ coronaboard-web/gatsby-node.js
@@ -2,23 +2,11 @@ const { getDataSource } = require('./src/data-loader');
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
-
-  let dataSource;
-  try {
-    // 데이터 가져오기
-    dataSource = await getDataSource();
-  } catch (err) {
-    console.warn('오늘 데이터가 없어서 기본 데이터를 사용합니다.');
-    // 오늘 데이터 없으면 빈 배열로 초기화
-    dataSource = {
-      globalStats: [],
-      countryStats: [],
-    };
-  }
+  const dataSource = await getDataSource();
 
   createPage({
     path: '/',
-    component: require.resolve('./src/templates/single-page.js'),
+    component: require.resolve('./src/template/single-page.js'),
     context: { dataSource },
   });
 };
