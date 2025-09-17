452cc478df7c2e57329a20921ab9b9e9bd293bc8
4fvsx3 <4fvsx3@gmail.com>
2025-09-18 08:47:46 +0900

    Update gatsby-config.js

--- coronaboard-web/gatsby-config.js
+++ coronaboard-web/gatsby-config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://davidshim.kr',
+    title: '통계표 테스팅(COVID)',
+    description:
+      '다양한 최신 기술들을 공부하기 위해, 기존 웹개발자가 연습한 페이지(coronaboard.kr)',
+    image: '/redheart.png', // SNS/SEO 공유용
+  },
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-sitemap',
@@ -22,15 +29,8 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `/redheart.png`, // ← favicon 경로 (static 폴더 안)
+        icon: `static/redheart.png`, // static 폴더 안에 아이콘 넣기
       },
     },
   ],
-  siteMetadata: {
-    siteUrl: 'https://davidshim.kr',
-    title: '통계표 테스팅(COVID)',
-    description:
-      '다양한 최신 기술들을 공부하기 위해, 기존 웹개발자가 연습한 페이지(coronaboard.kr)',
-    image: '/redheart.png', // SNS/SEO 공유용
-  },
 };
