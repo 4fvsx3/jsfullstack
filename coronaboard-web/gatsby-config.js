8da8450dbec6963c4a5e8c64c2f3cd6085f0e124
4fvsx3 <4fvsx3@gmail.com>
2025-09-17 17:14:14 +0900

    Update gatsby-config.js

--- coronaboard-web/gatsby-config.js
+++ coronaboard-web/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   siteMetadata: {
     siteUrl: 'https://coronaboard.kr',
-    title: '통계표 테스팅(COVID),
+    title: '통계표 테스팅(COVID)',
     description:
       '다양한 최신 기술들을 공부하기 위해, 기존 웹개발자가 연습한 페이지(coronaboard.kr)',
     image: 'src/static/red heart.png',
