de4ebae25c4b824e8ab54558653aadaad46058d0
4fvsx3 <4fvsx3@gmail.com>
2025-09-17 12:22:49 +0900

    Update single-page.js

--- coronaboard-web/src/templates/single-page.js
+++ coronaboard-web/src/templates/single-page.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { css } from '@emotion/react';
 import { Dashboard } from '../components/dashboard';
@@ -12,17 +12,19 @@ import { YoutubeSlide } from '../components/youtube-slide';
 import HelmetWrapper from '../components/helmet-wrapper';
 
 export default function SinglePage({ pageContext }) {
-  const { dataSource } = pageContext;
-  const { lastUpdated, globalStats, notice } = dataSource;
-  // 사용자의 언어/지역 설정에 맞는 날짜 형태로 표시
+  // 안전하게 기본값 처리
+  const dataSource = pageContext.dataSource || {};
+  const lastUpdated = dataSource.lastUpdated || new Date().toISOString();
+  const globalStats = Array.isArray(dataSource.globalStats) ? dataSource.globalStats : [];
+  const notice = Array.isArray(dataSource.notice) ? dataSource.notice : [];
+
   const lastUpdatedFormatted = new Date(lastUpdated).toLocaleString();
-
-
 
   return (
     <div id="top">
       <HelmetWrapper title={'홈'} />
-      {/* 상단 검은색 배경 만들기 */}
+
+      {/* 상단 검은색 배경 */}
       <div
         css={css`
           position: absolute;
@@ -32,6 +34,7 @@ export default function SinglePage({ pageContext }) {
           z-index: -99;
         `}
       />
+
       <h1
         css={css`
           padding-top: 48px;
@@ -45,6 +48,7 @@ export default function SinglePage({ pageContext }) {
         <br />
         통계판 만들어 보기
       </h1>
+
       <p className="text-center text-white">
         마지막 업데이트: {lastUpdatedFormatted}
       </p>
@@ -53,22 +57,20 @@ export default function SinglePage({ pageContext }) {
       <Notice notice={notice} />
 
       <div
-         css={css`
-           display: flex;
-            justify-content: center; /* 수평 중앙 정렬 */
-           align-items: center;     /* 수직 중앙 정렬 */
-            height: 100px;           /* 부모 높이 지정 필요 */
-          `}
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100px;
+        `}
       >
         {/* square01 */}
-
       </div>
 
       <Navigation />
 
-      {/* 각 슬라이드에 지정된 id 값은 Navigation 컴포넌트 안의 Link에 지정된 to 값과 동일해야함 */}
+      {/* 각 슬라이드에 지정된 id 값은 Navigation 안의 Link와 동일해야 함 */}
       <GlobalSlide id="global-slide" dataSource={dataSource} />
-
       <iframe
         src="https://ads-partners.coupang.com/widgets.html?id=637932&template=carousel&trackingCode=AF0322893&subId=&width=100%25&height=200"
         width="100%"
@@ -77,7 +79,6 @@ export default function SinglePage({ pageContext }) {
         scrolling="no"
         referrerPolicy="unsafe-url"
       ></iframe>
-
       <GlobalChartSlide id="global-chart-slide" dataSource={dataSource} />
       <KoreaChartSlide id="korea-chart-slide" dataSource={dataSource} />
       <YoutubeSlide id="youtube-slide" dataSource={dataSource} />
