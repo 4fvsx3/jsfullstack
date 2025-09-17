2444befd7208c13e13a6d3bd88f5dc5027835b30
4fvsx3 <4fvsx3@gmail.com>
2025-09-17 12:13:52 +0900

    Update single-page.js

--- coronaboard-web/src/templates/single-page.js
+++ coronaboard-web/src/templates/single-page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { css } from '@emotion/react';
 import { Dashboard } from '../components/dashboard';
@@ -12,16 +12,12 @@ import { YoutubeSlide } from '../components/youtube-slide';
 import HelmetWrapper from '../components/helmet-wrapper';
 
 export default function SinglePage({ pageContext }) {
-  const { dataSource } = pageContext || {};
-  const {
-    lastUpdated = null,
-    globalStats = {},
-    notice = {},
-  } = dataSource || {}; // dataSource가 null이면 기본값 사용
+  const { dataSource } = pageContext;
+  const { lastUpdated, globalStats, notice } = dataSource;
+  // 사용자의 언어/지역 설정에 맞는 날짜 형태로 표시
+  const lastUpdatedFormatted = new Date(lastUpdated).toLocaleString();
 
-  const lastUpdatedFormatted = lastUpdated
-    ? new Date(lastUpdated).toLocaleString()
-    : '데이터 없음';
+
 
   return (
     <div id="top">
@@ -36,7 +32,6 @@ export default function SinglePage({ pageContext }) {
           z-index: -99;
         `}
       />
-
       <h1
         css={css`
           padding-top: 48px;
@@ -50,27 +45,29 @@ export default function SinglePage({ pageContext }) {
         <br />
         통계판 만들어 보기
       </h1>
-
       <p className="text-center text-white">
         마지막 업데이트: {lastUpdatedFormatted}
       </p>
 
-      {/* 데이터가 없으면 빈 객체로 보내서 Dashboard와 Notice도 에러 방지 */}
       <Dashboard globalStats={globalStats} />
       <Notice notice={notice} />
 
       <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100px;
-        `}
-      ></div>
+         css={css`
+           display: flex;
+            justify-content: center; /* 수평 중앙 정렬 */
+           align-items: center;     /* 수직 중앙 정렬 */
+            height: 100px;           /* 부모 높이 지정 필요 */
+          `}
+      >
+        {/* square01 */}
+
+      </div>
 
       <Navigation />
 
-      <GlobalSlide id="global-slide" dataSource={dataSource || {}} />
+      {/* 각 슬라이드에 지정된 id 값은 Navigation 컴포넌트 안의 Link에 지정된 to 값과 동일해야함 */}
+      <GlobalSlide id="global-slide" dataSource={dataSource} />
 
       <iframe
         src="https://ads-partners.coupang.com/widgets.html?id=637932&template=carousel&trackingCode=AF0322893&subId=&width=100%25&height=200"
@@ -81,19 +78,9 @@ export default function SinglePage({ pageContext }) {
         referrerPolicy="unsafe-url"
       ></iframe>
 
-      <GlobalChartSlide id="global-chart-slide" dataSource={dataSource || {}} />
-
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100px;
-        `}
-      ></div>
-
-      <KoreaChartSlide id="korea-chart-slide" dataSource={dataSource || {}} />
-      <YoutubeSlide id="youtube-slide" dataSource={dataSource || {}} />
+      <GlobalChartSlide id="global-chart-slide" dataSource={dataSource} />
+      <KoreaChartSlide id="korea-chart-slide" dataSource={dataSource} />
+      <YoutubeSlide id="youtube-slide" dataSource={dataSource} />
 
       <Footer />
     </div>
