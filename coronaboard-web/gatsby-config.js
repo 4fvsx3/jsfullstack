46d37d4eb8b7184b368339ab8306c907c822d925
4fvsx3 <4fvsx3@gmail.com>
2025-09-18 08:59:55 +0900

    Update gatsby-config.js

--- coronaboard-web/gatsby-config.js
+++ coronaboard-web/gatsby-config.js
@@ -19,6 +19,6 @@ module.exports = {
     title: '다양한 최신 기술들을 공부하기 위해, 기존 웹개발자가 연습한 페이지(coronaboard.kr)',
     description:
       '코로나19에 관한 세계 각 국가들의 통계 및 뉴스 등을 취합하여 실시간 정보와 다양한 차트를 제공합니다',
-    image: 'https://coronaboard.kr/ogimage.png',
+    image: '/redheart.png',
   },
 };
